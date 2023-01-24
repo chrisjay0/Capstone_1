4fd1028a2d124f9632901dd1d1527af287006cec
Alttaab <alttaab@gmail.com>
2023-01-23 16:16:55 -0800

    Initializing repository with boilerplates and directory structure.

---
+++ mythodex/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+
+import requests
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = False
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+app.config['SECRET_KEY'] = 'SECRET'
+toolbar = DebugToolbarExtension(app)
+
+
+##############################################################################
+# Homepage and error pages
+
+
+@app.route('/')
+def show_homepage():
+    res = requests.get("https://www.dnd5eapi.co/api/magic-items/adamantine-armor/").json()
+    
+    return render_template('home.html',response=res)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """404 NOT FOUND page."""
+
+    return render_template('404.html'), 404

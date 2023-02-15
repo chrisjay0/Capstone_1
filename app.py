e34ab001da20874d86bc058f789e34118ad49a83
Alttaab <alttaab@gmail.com>
2023-02-15 13:07:21 -0800

    Adding code to accept and modify generated heroku database url.

--- app.py
+++ app.py
@@ -20,8 +20,11 @@ from users.services import User, authenticate_user, UserService
 app = Flask(__name__)
 # modal = Modal(app)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
-    'DATABASE_URL', 'postgresql:///mythodex')
+heroku_uri = os.environ.get('DATABASE_URL')
+if heroku_uri:
+    app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql'+heroku_uri[8:]
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql:///mythodex'
 
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_ECHO"] = False
